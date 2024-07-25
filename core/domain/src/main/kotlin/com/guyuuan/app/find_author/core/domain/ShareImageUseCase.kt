d38cd1f535f8c5ff6ef3f03ecebaa7b1995d8809
guyuuan <chunjinchen1998@gmail.com>
2024-07-25 23:41:45 +0800

    feat: support share image

--- core/domain/src/main/kotlin/com/guyuuan/app/find_author/core/domain/ShareImageUseCase.kt
+++ core/domain/src/main/kotlin/com/guyuuan/app/find_author/core/domain/ShareImageUseCase.kt
@@ -7,7 +7,6 @@ import androidx.core.net.toUri
 import com.guyuuan.app.find_author.core.common.R
 import com.guyuuan.app.find_author.core.data.BucketRepository
 import com.guyuuan.app.find_author.core.data.model.ImageItem
-import com.guyuuan.app.find_author.core.database.model.BucketType
 import java.io.File
 import javax.inject.Inject
 
@@ -22,10 +21,10 @@ class ShareImageUseCase @Inject constructor(
     suspend operator fun invoke(image: ImageItem, context: Context) {
         val bucket =
             bucketRepository.getBucket(image.bucketId) ?: throw RuntimeException("bucket not found")
-        val bucketType = bucket.type
         val file = context.contentResolver.openInputStream(image.uri.toUri())?.use {
             val cacheDir = context.externalCacheDir ?: context.cacheDir
-            val dir = File(cacheDir, "images${File.separatorChar}${bucket.name}${File.separatorChar}")
+            val dir =
+                File(cacheDir, "images${File.separatorChar}${bucket.name}${File.separatorChar}")
             dir.mkdirs()
             val file = File(dir, image.name)
             file.createNewFile()
@@ -38,13 +37,9 @@ class ShareImageUseCase @Inject constructor(
             return
         }
         val shareUri = FileProvider.getUriForFile(context, "${context.packageName}.share", file)
-        if (bucketType == BucketType.MediaStore) {
-            context.startActivity(Intent.createChooser(Intent(Intent.ACTION_SEND).apply {
-                putExtra(Intent.EXTRA_STREAM, shareUri)
-                type = "image/*"
-            }, context.getString(R.string.share_title)))
-        } else if (bucketType == BucketType.SAF) {
-
-        }
+        context.startActivity(Intent.createChooser(Intent(Intent.ACTION_SEND).apply {
+            putExtra(Intent.EXTRA_STREAM, shareUri)
+            type = "image/*"
+        }, context.getString(R.string.share_title)))
     }
 }
